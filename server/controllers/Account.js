c52b652a5e64737045043137e4502ad6d1c3ce93
Finn Marable <fbm5192@g.rit.edu>
2025-11-09 18:30:00 -0500

    domomakerBfirstpush

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -5,7 +5,8 @@ const { Account } = models;
 const loginPage = (req, res) => res.render('login');
 const signupPage = (req, res) => res.render('signup');
 const logout = (req, res) => {
-
+   req.session.destroy();
+   res.redirect('/');
 };
 const login = (req, res) => {
   const username = `${req.body.username}`;
@@ -17,6 +18,8 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(401).json({ error: 'wrong username or password' });
     }
+
+    req.session.account = Account.toAPI(account);
     return res.json({ redirect: '/maker' });
   });
 };
@@ -36,6 +39,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) {
     console.log(err);
